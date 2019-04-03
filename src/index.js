88d64c2106c2a81d00c44bf62d779f2322459258
William Guitarrari <williamguitarrari@gmail.com>
2019-04-03 17:12:25 -0300

    envio do projeto

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import Routes from './routes';
+
+import "./config/StatusBarConfig";
+
+// const instructions = Platform.select({
+//   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+//   android:
+//     'Double tap R on your keyboard to reload,\n' +
+//     'Shake or press menu button for dev menu',
+// });
+
+const App = () => <Routes />
+
+// export default class App extends Component {
+//   render() {
+//     return (
+//       <Routes />
+//     );
+//   }
+// }
+
+export default App;
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: '#F5FCFF',
+//   },
+//   welcome: {
+//     fontSize: 20,
+//     textAlign: 'center',
+//     margin: 10,
+//   },
+//   instructions: {
+//     textAlign: 'center',
+//     color: '#333333',
+//     marginBottom: 5,
+//   },
+// });
